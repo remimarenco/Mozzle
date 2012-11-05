a4b6df7527191b32aec1c06475d4e06d10ccec2f
romainPrignon <pro.rprignon@gmail.com>
2012-11-05 10:35:15 +0100

    Ajout des dossiers de base

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Lorsque le document est pret ...
+$(document).ready(function(){
+
+});
+
+// Lorsque Jquery mobile est pret ...
+$(document).bind("mobileinit", function(){
+  
+});
+
+// Lorsque Phonegap est pret ...
+window.addEventListener('load', function () {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}, false);
+
+function onDeviceReady(){
+    
+}
+	
