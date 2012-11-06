7775c793ec2ccd0c5fe5557be81d1001b9d1a67e
Hedi TARCHOUNI <hedi.tarchouni@gmail.com>
2012-11-06 16:55:07 +0100

    verif fin puzzle + resize

--- drag-and-drop-puzzle.js
+++ drag-and-drop-puzzle.js
@@ -2,6 +2,13 @@
 // check for drag and drop support
 var iOS = !!navigator.userAgent.match('iPhone OS') || !!navigator.userAgent.match('iPad');
 
+//nombre piece du puzzle
+sessionStorage.setItem("nomprePiece",4);
+
+//on initialisele tableau contenant l'etat des places
+for (i=0; i<sessionStorage.getItem("nomprePiece"); i++) {
+	sessionStorage.setItem("place"+i,0);
+}
 // set images array
 var images = [];
 var piece = "";
@@ -30,19 +37,61 @@ for (i=0; i<9; i++) {
 }
 // add drag and drop functions on the frame divs
 
+
+
+ resize();
+ 
+ 
 function drag(draggableitem, e) {
 	e.dataTransfer.setData("Text", draggableitem.id);	
 }
 function drop(target, e) {
 	var id = e.dataTransfer.getData('Text');
+	
+	//on recupere dans le local storage la place de depart de la piece
 	var placeOfPiece = sessionStorage.getItem(id);
 	alert(placeOfPiece +" "+target.id);
 	
+	//on compare la place de la piece et la place ou elle doit etre deplacée
 	if (placeOfPiece==target.id)
 	{
 		target.appendChild(document.getElementById(id));
 		
+		//on indique que cet emplacement est correctement remplit
+		sessionStorage.setItem(target.id,1);
 	}
-	e.preventDefault();
+	
 
+	e.preventDefault();
+	//on parcourt le local storage de toute les places pour verifier si la partie n'est pas finie
+	var estFinie=1;
+
+	for (i=0; i<sessionStorage.getItem("nomprePiece"); i++) {
+
+		//si une place n'est pas occupé la partie est finie
+		if (sessionStorage.getItem("place"+i)==0) 
+		{
+			estFinie=0;
+		}
+		
+	}
+
+	if(estFinie==1)
+	{
+		alert('gagneeeeeee');
+	}
+
+}
+
+function resize() {
+	var largeur=screen.availWidth*0.8;
+	document.getElementById('puzzle-frame2').style.height=(largeur)+'px';
+	document.getElementById('puzzle-frame2').style.width=(largeur)+'px';
+	for (i=0; i<sessionStorage.getItem("nomprePiece"); i++) {
+		document.getElementById('piece'+i).style.height=(largeur/2)+'px';
+		document.getElementById('piece'+i).style.width=(largeur/2)+'px';
+		document.getElementById('place'+i).style.height=(largeur/2)+'px';
+		document.getElementById('place'+i).style.width=(largeur/2)+'px';
+	}
+		
 }
