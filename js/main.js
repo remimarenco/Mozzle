b7b0c5bff31b683b47da46cbdc7ceca234903ee7
romainPrignon <pro.rprignon@gmail.com>
2012-11-06 13:37:30 +0100

    Ajout js du drap and drop du jeux mot

--- js/main.js
+++ js/main.js
@@ -35,3 +35,48 @@ $( '#mot-niveau-1' ).live( 'pageshow',function(event){
 //  sinon on parcourt le mot
 //   si c'est le bon style (ou bonne valeur pour chache case)
 //      onjue le son de gagné
+
+
+
+
+
+
+
+
+
+// set images array
+var images = [];
+var piece = "";
+var place = ""; 
+for (i=0; i<12; i++) {
+    j = i+1;
+    images[i] = "puzzle-piece" + j + ".png";    
+}
+// randomize the pieces to display
+images.sort(function() {return 0.5 - Math.random()});
+for (i=0; i<12; i++) {
+    $('#pieces').append("<img src=\"/examples/images/puzzle/"+images[i]+"\" id=\"piece"+i+"\" draggable=true ondragstart=\"drag(this, event);\">");
+
+    // iPhone and iPad functionality
+    if (iOS) {
+        piece = "piece"+i;
+        place = "place"+i;
+        $("#piece"+i).css('float','left');
+        new webkit_draggable(piece, {revert : false, scroll : true} );
+        webkit_drop.add(place, {onDrop : function() { 
+            $("#place"+i).append(piece);
+        }
+        });
+    }
+
+}
+// add drag and drop functions on the frame divs
+
+function drag(draggableitem, e) {
+    e.dataTransfer.setData("Text", draggableitem.id);   
+}
+function drop(target, e) {
+    var id = e.dataTransfer.getData('Text');
+    target.appendChild(document.getElementById(id));
+    e.preventDefault();
+}
