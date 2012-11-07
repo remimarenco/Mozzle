cca8c71d0f98be0ad89377918d3390dfb16a7368
romainPrignon <pro.rprignon@gmail.com>
2012-11-07 09:28:44 +0100

    Jeux mot niveau 1 fonctionnel. Il manque la validation et la gestion de lettre doublon

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function onDeviceReady(){
 // TODO a wrap dans un module javascript
 var nbDragAction = 1;   // Nombre de fois que l'on peux jouer (autant que de lettre + 1)
 
-// TODO Declenchement du jeude mot niveau i avec controle de la difficulté dans la fonction
+// TODO Declenchement du jeu de mot niveau i avec controle de la difficulté dans la fonction
 // Déclenchement du jeu mot niveau 1
 $( '#mot-niveau-1' ).live( 'pageshow',function(event){
     
@@ -28,7 +28,7 @@ $( '#mot-niveau-1' ).live( 'pageshow',function(event){
     var wordLength = word['libelle'].length;
     
     var nbFirstLetter = Math.floor(wordLength / 3);     // On récupère un nombre de lettre a afficher suivant la taille du mot
-    
+    var lettreRestante = new Array();
     
     $.each(word['lettres'],function(i,val){
         // TODO
@@ -39,15 +39,15 @@ $( '#mot-niveau-1' ).live( 'pageshow',function(event){
         if(i<=nbFirstLetter){
             $('#ordre ul').append("<li id='"+val+"'>"+val+"</li>");    // On ajoute les premieres lettres en noire
         }else{
-            $('#ordre ul').append("<li id='"+val+"' style='background-color:#222;width:16px;height:16px;' ondragenter='return false;' ondragover='return false;' ondrop=\'drop_mot(this, event)\'> </li>");    // On ajoute les indications pour facile et moyen
+            lettreRestante.push(val);
+            $('#ordre ul').append("<li id='"+val+"' class='font-white' ondragenter='return false;' ondragover='return false;' ondrop=\'drop_mot(this, event)\'>"+val+"</li>");    // On ajoute les indications pour facile et moyen
         }
     });
 
     // Dans tout les cas,on affiche les lettres dans le desordre
-    $.each(word['lettres'],function(i,val){
-        if(i>nbFirstLetter){
-            $('#desordre ul').append("<li id='"+val+"' draggable='true' ondragstart=\'drag_mot(this, event)\'>"+val+"</li>");    // On ajoute les lettres restantes
-        }
+    lettreRestante.sort(function() { return 0.5 - Math.random() });
+    $.each(lettreRestante,function(i,val){
+        $('#desordre ul').append("<li id='desordre_"+val+"' draggable='true' ondragstart=\'drag_mot(this, event)\'>"+val+"</li>");    // On ajoute les lettres restantes        
         nbDragAction++;     // Il y a autant d'action que de nombre de lettre dans le mot + 1
     });
 
@@ -60,14 +60,23 @@ function getOneRandomWord(){
 }
 
 function drag_mot(draggableitem, e) {
-    e.dataTransfer.setData("lettre", $(draggableitem).attr('id'));
+    var lettre = $(draggableitem).attr('id');
+    lettre = lettre.replace('desordre_','');
+    e.dataTransfer.setData("lettre", lettre);
 }
 
 function drop_mot(target, e) {
+    if(nbDragAction==0){
+        alert('Perdu '); // TODO a changer par une popup p-e
+        // Jouer son perdu
+    }
     var lettre = e.dataTransfer.getData('lettre');
     var target = $(target);
+    var source = $('#desordre_'+lettre);
     if(target.attr('id')==lettre){
         target.html(lettre);
+        target.removeClass('font-white');
+        source.remove();
         e.preventDefault();
     }
     nbDragAction--;     // On décrémente le nombre de drag restant
