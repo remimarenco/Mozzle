8369214bee436c4025d524579aca2d8c540b7267
Hedi TARCHOUNI <hedi.tarchouni@gmail.com>
2012-11-08 15:48:11 +0100

    arret chrono quand partie finie+transition fin de partie

--- js/drag-and-drop-puzzle.js
+++ js/drag-and-drop-puzzle.js
@@ -162,7 +162,7 @@ function resize() {
 function partieGagnee()
 {
 	$("#popupGagnePuzzle"+localStorage.getItem("niveau")).popup("open");
-	$('body').append("<audio autoplay><source src='res/audio/animaux/Fr-chat.ogg' type='audio/ogg'></audio>");
+	//$('body').append("<audio autoplay><source src='res/audio/animaux/Fr-chat.ogg' type='audio/ogg'></audio>");
 	
 
 	
